cc224efa5e3dbf3af4d77d873c5a49a4d069a61a
Emilio Alejandro <alejandro9445@outlook.com>
2015-11-07 01:24:35 -0800

    Cambio de fondo de pantalla.

--- GEH_OSSimulator/UserControls/Desktop.xaml.cs
+++ GEH_OSSimulator/UserControls/Desktop.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GEH_OSSimulator.ViewModels;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,37 @@ namespace GEH_OSSimulator.UserControls
     /// </summary>
     public partial class Desktop : UserControl
     {
+        private DesktopViewModel _viewModel;
+        public DesktopViewModel ViewModel { 
+            get {
+                if (_viewModel == null)
+                    _viewModel = new DesktopViewModel();
+                return _viewModel;
+            }
+            set {
+                _viewModel = value;
+            }
+        }
         public Desktop()
         {
             InitializeComponent();
+            this.DataContext = ViewModel;
+            
+        }
+
+        private void Grid_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|";
+            if (fileDialog.ShowDialog() == true) {
+                var backgroundImage = new Image();
+                backgroundImage.Source = new BitmapImage(new Uri(fileDialog.FileName));
+                var imageBrush = new ImageBrush();
+                imageBrush.ImageSource = backgroundImage.Source;
+                ViewModel.BackgroundGrid.Background = imageBrush;
+            }
+                
+
         }
     }
 }
